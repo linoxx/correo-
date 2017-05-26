a994b0b6c9d29a138962229473bed6293cdab3ab
linuxsurfer23 <alineprincess478@gmail.com>
2017-05-27 00:54:11 +0200

    cambio de correo
    manuel ortiz

---
+++ EnviaCorreosXamarinApp-master/EjercicioBase/EnviarCorreo.Nativo/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using EnviarCorreo.Nativo.Services;
+
+namespace EnviarCorreo.Nativo
+{
+    [Activity(Label = "EnviarCorreo.Nativo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button actionButton;
+        string emailBase = "manuelortizfortis@gmail.com";
+        string codeBase = "iniciativa";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            actionButton = FindViewById<Button>(Resource.Id.btnReportar);
+            actionButton.Click += btnReportar_Click;
+        }
+
+        private async void btnReportar_Click(object sender, EventArgs e)
+        {
+            ServiceHelper serviceHelper = new ServiceHelper();
+            if (emailBase == "manuelortizfortis@gmail.com" || codeBase == "iniciativa")
+            {
+                
+
+            }
+            actionButton.Enabled = false;
+            await serviceHelper.InsertarEntidad("manuelortizfortis@gmail.com", "iniciativa");
+            actionButton.Text = "Reporte enviado";
+
+            Toast.MakeText(this, "Tienes correo nuevo!", ToastLength.Short).Show();
+        }
+    }
+}
+
